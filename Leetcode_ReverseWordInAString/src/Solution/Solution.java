f54fc2466949130220f5075dec55f5b6ba9e1678
Bonan Liu <liubonan@liubonan.com>
2014-06-18 23:35:44 -0700

    Leetcode Reverse Words in a String

---
+++ Leetcode_ReverseWordInAString/src/Solution/Solution.java
@@ -0,0 +1,63 @@
+package Solution;
+
+public class Solution {
+
+	public String reverseWords(String s) {
+        if(s == null){
+        	return null;
+        }
+
+        char[] sb = new char[s.length()];
+        for(int i = s.length() - 1; i >= 0; i--){
+        	sb[s.length() - i - 1] = s.charAt(i);
+        }
+        
+        int j = 0;
+        for(int i = 0; i < s.length(); i++){
+        	if(sb[i] == ' '){
+        		this.invertSingleWord(sb, j, i-1);
+        		j = i + 1;
+        	}
+        	else if(i == s.length() - 1){
+        		this.invertSingleWord(sb, j, i);
+        	}
+        }
+        
+        StringBuffer str = new StringBuffer();
+        
+        boolean needSpace = false;
+        int i = 0;
+        
+        while(i < sb.length && sb[i] == ' '){
+        	i++;
+        }
+        
+        for(; i < sb.length; i++){
+        	char c = sb[i];
+        	if(c == ' '){
+        		needSpace = true;
+        	}
+        	else{
+        		if(needSpace){
+        			str.append(' ');
+        			needSpace = false;
+        		}
+        		str.append(c);
+        	}
+        }
+        
+        return str.toString();
+    }
+	
+	private char[] invertSingleWord(char[] s, int i, int j){
+		while(i < j){
+			char temp = s[i];
+			s[i] = s[j];
+			s[j] = temp;
+			i++;
+			j--;
+		}
+		
+		return s;
+	}
+}
---
+++ Leetcode_ReverseWordInAString/src/Solution/UnitTests.java
@@ -0,0 +1,20 @@
+package Solution;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class UnitTests {
+	
+	@Test
+    public void thisAlwaysPasses() {
+		Solution sol = new Solution();
+		assertEquals("blue is sky the", sol.reverseWords(" the  sky is blue "));
+
+    }
+
+}
